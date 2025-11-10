c7df97cdd4d2b124e422fb0bc0fc4136d20a75e8
chang <chang@DESKTOP-J4HD04Q>
2025-11-10 14:19:26 -0500

    initial setup for chessTournament recoding plus init players and player array

--- chessTournament/src/main/Player.java
+++ chessTournament/src/main/Player.java
@@ -1,5 +1,37 @@
 package main;
 
 public class Player {
+	
+	
+	public String playerName;
+	
+	public int playerId;
+	public int score;
+	
+	public boolean inGame;
+	public boolean playerIsCpu;
+	
+	public Player(String name, boolean isCpu, int[] tournamentPlayerIds){
+		
+		playerName = name;
+		
+		playerIsCpu = isCpu;
+		
+		inGame = false;
+		
+		for(int i = 0; i < tournamentPlayerIds.length; i++) {
+			
+			if(tournamentPlayerIds[i] == 0) {
+				
+				tournamentPlayerIds[i] = i+1;
+				
+				break;
+			}
+			
+		}
+		
+	}
+	
+	
 
 }
---
+++ chessTournament/src/main/chessTournament.java
@@ -0,0 +1,20 @@
+package main;
+
+public class chessTournament {
+	
+	
+	public static Player[] tournamentPlayers = new Player[8];
+	public static int[] tournamentPlayerIds = new int[8];
+
+	public static void main(String[] args) {
+		
+		
+		for(int i = 0; i < (tournamentPlayers.length - 1); i++) {
+			tournamentPlayers[i] = new Player( "CPU " + (i + 1), true, tournamentPlayerIds);
+		}
+		
+
+	}
+
+	
+}
